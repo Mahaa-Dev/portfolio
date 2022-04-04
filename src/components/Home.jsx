028ae0c5c00a6a0399a81e65f46f9a14c3acb5e8
Mahaa-Dev <np03a190201@heraldcollege.edu.np>
2022-04-04 14:41:48 +0545

    navbar updated

---
+++ src/components/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import {HiArrowNarrowRights} from 'react-icons/hi'
+
+const Home = () => {
+  return (
+    <div>Home</div>
+  )
+}
+
+export default Home
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,12 @@
-import React from 'react'
-import {faBars, faTimes} from 'react-icons/fa'
+import React, {useState} from 'react'
+import {FaBars, FaTimes} from 'react-icons/fa'
 import Logo from '../Assets/logo.png'
 
 const Navbar = () => {
+  const[nav, setNav] = useState(false)
+  const handleclick = () =>setNav(!nav)
+
+  
   return (
     <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300">
         <div>
@@ -12,8 +16,8 @@ const Navbar = () => {
         </div>
 {/*menu */}
 
-        <div>
-            <ul>
+        <div className='hidden md:flex'>
+            <ul className='hidden md:flex'>
                 <li>Home</li>
                 <li>About</li>
                 <li>Skills</li>
@@ -22,15 +26,26 @@ const Navbar = () => {
             </ul>
         </div>
         {/*Hamburger*/}
-        <div>
-
+        <div onClick= {handleclick} className='md:hidden z-10'>
+         {!nav ? <FaBars/> : <FaTimes/>}
         </div>
 
         {/* Mobile Menu */}
 
-        <div>
+        <div className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'}>
+        <ul>
+                <li className='py-6 text-4xl'>Home</li>
+                <li className='py-6 text-4xl'>About</li>
+                <li className='py-6 text-4xl'>Skills</li>
+                <li className='py-6 text-4xl'>Work</li>
+                <li className='py-6 text-4xl'>Contact</li>
+            </ul>
+
             
         </div>
+
+        {/*social icons*/}
+        <div className='flex fixed flex-col top-[35%] left-0'></div>
 
     </div>
   )
